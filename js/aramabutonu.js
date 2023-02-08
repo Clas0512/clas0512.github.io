23de3828b7d9e7fcfae747a26115527d53b870de
Clas0512 <mrb.bn@hotmail.com>
2023-02-08 04:06:19 +0300

    search button adding

--- js/aramabutonu.js
+++ js/aramabutonu.js
@@ -3,8 +3,8 @@ function searchSite() {
     var searchValue = document.getElementById("searchInput").value;
 
     // Aranacak değere göre sayfanın yönlendirilmesi
-    if (searchValue === "hakkımızda") {
-      location.href = "https://example.com/about";
+    if (searchValue === "Adana") {
+      location.href = "https://www.google.com";
     } else if (searchValue === "iletisim") {
       location.href = "https://example.com/contact";
     } else {
